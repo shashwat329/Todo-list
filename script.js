d329c06648e357b664118e8ff9074cc07a52d709
shashwat singh <shashwatsingh@shashwats-MacBook-Air.local>
2023-07-03 23:26:26 +0530

    added feather

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let removeclick = document.getElementsByClassName('remove');
 // creating the function to add new listing
 function addtask() {
     let task = prompt("enter the task");
-    console.log(task);
+    if(task != ""){
     let node = document.createElement("li");
     let newiteam = document.createElement("input");
     newiteam.type = "checkbox";
@@ -16,4 +16,17 @@ function addtask() {
     console.log(node);
     node.append(task);
     document.getElementById("list").appendChild(node);
+    }
+    else{
+        alert("you have not entered the task");
+    }
+}
+// removing the let
+
+function removetask(){
+let index = prompt("enter the index of the the task");
+let list = document.getElementById("list");
+let li =list.getElementsByTagName("li")[index];
+li.parentNode.removeChild(li)[index];
+
 }
